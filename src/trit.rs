7193933543d5b09d06b99eae19565b0a3dc6ef30
Paul Gusmano <paul.gusmano@outlook.com>
2026-02-23 20:05:55 -0500

    Defining Not

--- src/trit.rs
+++ src/trit.rs
@@ -1,5 +1,34 @@
+use std::ops::Not;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Trit {
+    True,
+    Unknown,
+    False,
+}
+
+impl Not for Trit {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        match self {
+            Trit::True => Trit::False,
+            Trit::Unknown => Trit::Unknown,
+            Trit::False => Trit::True,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use rstest::rstest;
+
+    #[rstest]
+    #[case(Trit::True, Trit::False)]
+    #[case(Trit::Unknown, Trit::Unknown)]
+    #[case(Trit::False, Trit::True)]
+    fn test_not(#[case] input: Trit, #[case] expected: Trit) {
+        assert_eq!(!input, expected);
+    }
 }
